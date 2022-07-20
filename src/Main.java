5de8313f718433bb64dd6b83035b35c7712d7405
Mirlan1882 <108753857+Mirlan1882@users.noreply.github.com>
2022-07-20 23:27:03 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-public class Main {
 
+
+public class Main {
     public static void main(String[] args) {
-        Hero Buch = new Hero(100, 20, "fly");
-        Hero Killua = new Hero(100, 50);
         Boss boss1 = new Boss();
-        boss1.sethealth(300);
-        boss1.setdamage(50);
-        boss1.setsuperPower("Fly");
-        boss1.settypeDefence("Field");
-        System.out.println(boss1.getdamage() + " " + boss1.gethealth() + " " + boss1.gettypeDefence() + " " + boss1.getsuperPower());
+        Weapon weapon = new Weapon("fire", "gun");
+        boss1.setHealth(500);
+        boss1.setDamage(30);
+        boss1.setSuperPower("SuperJump");
+        boss1.setTypeDefence("Бронижелет");
     }
 }
