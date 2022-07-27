3371a25da87c034715491044e8274bc31b7e6022
Mirlan1882 <108753857+Mirlan1882@users.noreply.github.com>
2022-07-27 18:42:33 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
-
+package kg.geektech.game.general;
 
 public class Main {
-    public static void main(String[] args) {
-        Boss boss1 = new Boss();
-        Weapon weapon = new Weapon("fire", "gun");
-        boss1.setHealth(500);
-        boss1.setDamage(30);
-        boss1.setSuperPower("SuperJump");
-        boss1.setTypeDefence("Бронижелет");
-        boss1.setWeapon("fire", "gun");
-        System.out.println("Boss: " + boss1.getHealth() +  " " + boss1.getDamage() + " " + boss1.getTypeDefence() + " " + boss1.getSuperPower());
-    }
+
+    public static void main(String[] args) { RPG_Game.start(); }
 }
+
