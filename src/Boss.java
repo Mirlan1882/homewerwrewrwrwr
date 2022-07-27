ca76159873982799ae1454d3820fbbd4a843f46e
Mirlan1882 <108753857+Mirlan1882@users.noreply.github.com>
2022-07-27 18:42:08 +0600

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -1,15 +1,24 @@
+
+
 public class Boss extends GameEntity {
-    private Weapon WeaponBoss;
-
-    public Weapon getWeaponBoss() {
-        return WeaponBoss;
+    public Boss(int health, int damage) {
+        super(health, damage);
     }
 
-    public void setWeapon(Weapon Weapon) {
-        Weapon = WeaponBoss;
-    }
-
-    public void setWeapon(String fire, String gun) {
+    public void hit(Hero[] heroes) {
+        if (this.getHealth() > 0) {
+            for (int i = 0; i < heroes.length; i++) {
+                if (heroes[i].getHealth() > 0) {
+                    if (heroes[i] instanceof Berserk) {
+                        heroes[i].setHealth(heroes[i].getHealth()
+                                - this.getDamage() / 2);
+                        ((Berserk) heroes[i]).setSaved_damage(this.getDamage() / 2);
+                    } else {
+                        heroes[i].setHealth(heroes[i].getHealth()
+                                - this.getDamage());
+                    }
+                }
+            }
+        }
     }
 }
-
